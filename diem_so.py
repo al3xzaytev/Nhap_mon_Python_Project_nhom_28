9a656a4ec25c4fb76d964df56a7c7da425cdcea9
al3xzaytev <ieatsyrniks@gmail.com>
2024-04-21 00:30:34 +0700

    Bỏ việc vẽ bảng; sẽ do table_render.py làm

--- diem_so.py
+++ diem_so.py
@@ -1,13 +1,111 @@
 # Sắp xếp theo điểm môn 2
 
 import numpy as np
+import file_handling
+"""Global values below are placed so PyCharm does not go apeshit"""
+global np_dm1
+global np_mshv
+global np_mshv_sort
+global np_hthv_sort
 
-data_file = "hocvien.dat"
-file_hoc_vien = open(data_file, "r")
-dong = file_hoc_vien.readlines()
+global np_dm2
+global dm2_np_mshv
+global dm2_np_mshv_sort
+global dm2_np_hthv_sort
+global np_dm1_sort
+global np_dm2_after_dm1_sort
+
+flag_dm1 = False
+flag_dm2 = False
+flag_diem_so = False
+
+
+def initialization():
+    global flag_dm1, flag_dm2, flag_diem_so
+    flag_dm1 = False
+    flag_dm2 = False
+    flag_diem_so = False
+    import main as m
+    m.main()
+
+
+def dm1():
+    global flag_dm1
+    flag_dm1 = True
+    file_hoc_vien = open(file_handling.file_path(), "r")
+    dong = file_hoc_vien.readlines()
+
+    list_mshv = []
+    list_hthv = []
+    list_dm1 = []
+
+    for number_of_lines in dong:
+        danh_sach = number_of_lines.split(sep="|")
+        list_mshv.append(danh_sach[0])
+        list_hthv.append(danh_sach[1])
+        list_dm1.append(int(danh_sach[3]))
+
+    global np_dm1
+    global np_mshv
+    global np_mshv_sort
+    global np_hthv_sort
+
+    np_mshv = np.array(list_mshv)
+    np_hthv = np.array(list_hthv)
+    np_dm1 = np.array(list_dm1)
+
+    sort_args = np_dm1.argsort()
+
+    np_dm1 = np_dm1[sort_args]
+    np_mshv_sort = np_mshv[sort_args]
+    np_hthv_sort = np_hthv[sort_args]
+
+    import table_render as tr
+    tr.table_render()
+
+
+def dm2():
+    global flag_dm2
+    flag_dm2 = True
+
+    file_hoc_vien = open(file_handling.file_path(), "r")
+    dong = file_hoc_vien.readlines()
+
+    list_mshv = []
+    list_hthv = []
+    list_dm2 = []
+
+    for number_of_lines in dong:
+        danh_sach = number_of_lines.split(sep="|")
+        list_mshv.append(danh_sach[0])
+        list_hthv.append(danh_sach[1])
+        list_dm2.append(int(danh_sach[5]))
+
+    global np_dm2
+    global np_mshv
+    global np_mshv_sort
+    global np_hthv_sort
+
+    np_mshv = np.array(list_mshv)
+    np_hthv = np.array(list_hthv)
+    np_dm2 = np.array(list_dm2)
+
+    sort_args = np_dm2.argsort()
+
+    np_dm2 = np_dm2[sort_args]
+    np_mshv_sort = np_mshv[sort_args]
+    np_hthv_sort = np_hthv[sort_args]
+
+    import table_render as tr
+    tr.table_render()
 
 
 def diem_so():
+    global flag_diem_so
+    flag_diem_so = True
+    file_hoc_vien = open(file_handling.file_path(), "r")
+    dong = file_hoc_vien.readlines()
+
     list_mshv = []
     list_hthv = []
     list_dm1 = []
@@ -19,6 +117,14 @@ def diem_so():
         list_hthv.append(danh_sach[1])
         list_dm1.append(int(danh_sach[3]))
         list_dm2.append(int(danh_sach[5]))
+
+    global np_dm1
+    global np_dm2
+    global np_mshv
+    global np_mshv_sort
+    global np_hthv_sort
+    global np_dm1_sort
+    global np_dm2_after_dm1_sort
 
     np_mshv = np.array(list_mshv)
     np_hthv = np.array(list_hthv)
@@ -32,6 +138,11 @@ def diem_so():
     np_hthv_sort = np_hthv[sort_args_dm1]
     np_dm2_after_dm1_sort = np_dm2[sort_args_dm1]
 
+    import table_render as tr
+    tr.table_render()
+
+
+"""
     print()
     print("Bảng điểm học sinh theo thứ tự DM1-DM2 tăng dần:\n")
     print("|", '{:^14}'.format("Mã số học viên"), "|",
@@ -43,86 +154,10 @@ def diem_so():
 
     print()
 
-    for num_entries in range(0, len(np_mshv)):
-        print("|", '{:^14}'.format(np_mshv_sort[num_entries]), "|",
-              '{:<30}'.format(np_hthv_sort[num_entries]), "|",
-              '{:^12}'.format(np_dm1_sort[num_entries]), "|",
+    for num_entries in range(0, len(ds_np_mshv)):
+        print("|", '{:^14}'.format(ds_np_mshv_sort[num_entries]), "|",
+              '{:<30}'.format(ds_np_hthv_sort[num_entries]), "|",
+              '{:^12}'.format(ds_np_dm1_sort[num_entries]), "|",
               '{:^12}'.format(np_dm2_after_dm1_sort[num_entries]), "|",)
     print()
-
-
-def dm1():
-    list_mshv = []
-    list_hthv = []
-    list_dm1 = []
-
-    for number_of_lines in dong:
-        danh_sach = number_of_lines.split(sep="|")
-        list_mshv.append(danh_sach[0])
-        list_hthv.append(danh_sach[1])
-        list_dm1.append(int(danh_sach[3]))
-
-    np_mshv = np.array(list_mshv)
-    np_hthv = np.array(list_hthv)
-    np_dm1 = np.array(list_dm1)
-
-    sort_args = np_dm1.argsort()
-
-    np_dm1 = np.sort(np_dm1)
-    np_mshv_sort = np_mshv[sort_args]
-    np_hthv_sort = np_hthv[sort_args]
-
-    print()
-    print("Bảng điểm học sinh theo thứ tự DM1 tăng dần:\n")
-    print("|", '{:^14}'.format("Mã số học viên"), "|",
-          '{:<30}'.format("Họ tên"), "|",
-          '{:^12}'.format("Điểm môn 1"), "|")
-    for i in range(0, 66):
-        print("-", end="")
-
-    print()
-
-    for num_entries in range(0, len(np_mshv)):
-        print("|", '{:^14}'.format(np_mshv_sort[num_entries]), "|",
-              '{:<30}'.format(np_hthv_sort[num_entries]), "|",
-              '{:^12}'.format(np_dm1[num_entries]), "|",)
-    print()
-
-
-def dm2():
-    list_mshv = []
-    list_hthv = []
-    list_dm2 = []
-
-    for number_of_lines in dong:
-        danh_sach = number_of_lines.split(sep="|")
-        list_mshv.append(danh_sach[0])
-        list_hthv.append(danh_sach[1])
-        list_dm2.append(int(danh_sach[5]))
-
-    np_mshv = np.array(list_mshv)
-    np_hthv = np.array(list_hthv)
-    np_dm2 = np.array(list_dm2)
-
-    sort_args = np_dm2.argsort()
-
-    np_dm2 = np.sort(np_dm2)
-    np_mshv_sort = np_mshv[sort_args]
-    np_hthv_sort = np_hthv[sort_args]
-
-    print()
-    print("Bảng điểm học sinh theo thứ tự DM2 tăng dần:\n")
-    print("|", '{:^14}'.format("Mã số học viên"), "|",
-          '{:<30}'.format("Họ tên"), "|",
-          '{:^12}'.format("Điểm môn 2"), "|")
-
-    for i in range(0, 66):
-        print("-", end="")
-
-    print()
-
-    for num_entries in range(0, len(np_mshv)):
-        print("|", '{:^14}'.format(np_mshv_sort[num_entries]), "|",
-              '{:<30}'.format(np_hthv_sort[num_entries]), "|",
-              '{:^12}'.format(np_dm2[num_entries]), "|",)
-    print()
+"""
--- xem_danh_sach.py
+++ xem_danh_sach.py
@@ -1,3 +1,8 @@
+flag_ds = True
+
+global list_mshv, list_hthv, list_mmh1, list_dm1, list_mmh2, list_dm2
+
+
 def xem_danh_sach():
     # Danh sách sẽ theo form dưới đây:
     # MSHV|HTHV|MMH1|DM1|MMH2|DM2|
@@ -5,6 +10,8 @@ def xem_danh_sach():
     data_file = "hocvien.dat"
     file_hoc_vien = open(data_file, "r")
     dong = file_hoc_vien.readlines()
+
+    global list_mshv, list_hthv, list_mmh1, list_dm1, list_mmh2, list_dm2
 
     list_mshv = []
     list_hthv = []
@@ -25,39 +32,8 @@ def xem_danh_sach():
     print()
     print("Danh sách", data_file, "đã mở.")
     print()
-    print("|", '{:^14}'.format("Mã số học viên"), "|",
-          '{:<30}'.format("Họ tên"), "|",
-          '{:^14}'.format("Mã môn học 1"), "|",
-          '{:^12}'.format("Điểm môn 1"), "|",
-          '{:^14}'.format("Mã môn học 2"), "|",
-          '{:^12}'.format("Điểm môn 2"), "|")
+    import table_render as tr
+    tr.table_render()
 
-    ''' Cách format cũ
-    print('%-14s' % 'Ma so hoc vien', "|",
-          '%-30s' % "Ho ten hoc vien", "|",
-          '%-14s' % "Ma mon hoc 1", "|",
-          '%-12s' % "Diem mon 1", "|",
-          '%-14s' % "Ma mon hoc 2", "|",
-          '%-12s' % "Diem mon 2")
-    '''
-    for i in range(0, 115):
-        print("-", end="")
-    print()
-    for num_entries in range(0, len(list_mshv)):
-        print("|", '{:^14}'.format(list_mshv[num_entries]), "|",
-              '{:<30}'.format(list_hthv[num_entries]), "|",
-              '{:^14}'.format(list_mmh1[num_entries]), "|",
-              '{:^12}'.format(list_dm1[num_entries]), "|",
-              '{:^14}'.format(list_mmh2[num_entries]), "|",
-              '{:^12}'.format(list_dm2[num_entries]), "|")
-        ''' Cách format cũ
-        print('%-14s' % (list_mshv[num_entries]), "|",
-              '%-30s' % list_hthv[num_entries], "|",
-              '%-14s' % list_mmh1[num_entries], "|",
-              '%-12s' % list_dm1[num_entries], "|",
-              '%-14s' % list_mmh2[num_entries], "|",
-              '%-12s' % list_dm2[num_entries], " ")
-        '''
-    print()
     import main as m
     m.main()
