9a02c4f5b00f199b70021eb6c4cfc408beb1bbcd
al3xzaytev <ieatsyrniks@gmail.com>
2024-04-21 13:21:18 +0700

    Thêm check nếu file không tồn tại

--- file_handling.py
+++ file_handling.py
@@ -1,9 +1,22 @@
-def file_path():
+import os.path
+
+
+def get_file_path():
     default_file = "hocvien.dat"
 
-    file_path = str(input("Nhập đường dẫn file: "))
-
-    if file_path == "":
-        file_path = default_file
-    print("Đường dẫn đã chọn:", file_path)
-    return file_path
+    while True:
+        file_path = str(input("\nNhập đường dẫn file dữ liệu: "))
+        if file_path == "quit":
+            import main as m
+            m.intro()
+        elif file_path == "":
+            file_path = default_file
+            print("Đường dẫn đã chọn:", file_path)
+            return file_path
+        else:
+            print("Đường dẫn đã chọn:", file_path)
+            if not os.path.exists(file_path):
+                print("File không tồn tại!")
+            else:
+                print("Danh sách", file_path, "đã mở.")
+                return file_path
