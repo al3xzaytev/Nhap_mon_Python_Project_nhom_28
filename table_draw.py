e27ee2ce40b9aa1543f1069fdb469a13053ac84e
al3xzaytev <ieatsyrniks@gmail.com>
2024-04-21 21:03:31 +0700

    Thêm vẽ bảng cho tính năng dò tìm điểm theo mã môn học Tối ưu hoá lại hệ thống flag

--- table_draw.py
+++ table_draw.py
@@ -1,9 +1,5 @@
 # Phụ trách việc vẽ bảng
 
-import diem_so
-import xem_danh_sach
-
-
 def table_draw():
     print()
     print("|", '{:^14}'.format("Mã số học viên"), "|", end="")
@@ -11,17 +7,23 @@ def table_draw():
     print('{:<30}'.format("Họ tên"), "|", end="")
     print(" ", end="")
 
-    from diem_so import flag_dm1, flag_dm2, flag_diem_so
+    from diem_so import flag_dm1, flag_dm2, flag_diem_so, flag_mmh1, flag_mmh2
     from xem_danh_sach import flag_danh_sach
     if flag_dm1 is True:
         print('{:^11}'.format("Điểm môn 1"), "|")  # These must be 11 to be aligned for some reason????
-
     elif flag_dm2 is True:
         print('{:^11}'.format("Điểm môn 2"), "|")
 
+    elif flag_mmh1 is True:
+        print('{:^14}'.format("Mã môn học 1"), "|",
+              '{:^12}'.format("Điểm môn 1"), "|")
+    elif flag_mmh2 is True:
+        print('{:^14}'.format("Mã môn học 2"), "|",
+              '{:^12}'.format("Điểm môn 2"), "|")
+
     elif flag_diem_so is True:
         print('{:^11}'.format("Điểm môn 1"), "|",
-              '{:^12}'.format("Điểm môn 2"), "|", end="\n")  # This one is 12 ????
+              '{:^12}'.format("Điểm môn 2"), "|")  # This one is 12 ????
     elif flag_danh_sach is True:
         print('{:^14}'.format("Mã môn học 1"), "|",
               '{:^12}'.format("Điểm môn 1"), "|",
@@ -29,37 +31,45 @@ def table_draw():
               '{:^12}'.format("Điểm môn 2"), "|")
 
     print("|", end="")
-    for i in range(0, 16):
+    for i in range(0, 16):  # Mã số học viên
         print("-", end="")
     print("|", end="")
-    for i in range(0, 32):
+    for i in range(0, 32):  # Họ tên học viên
         print("-", end="")
     print("|", end="")
 
     if flag_dm1 is True or flag_dm2 is True:
-        for i in range(0, 13):
+        for i in range(0, 13):  # Điểm môn 1/2
+            print("-", end="")
+        print("|")
+
+    elif flag_mmh1 is True or flag_mmh2 is True:
+        for i in range(0, 16):  # Mã môn học
+            print("-", end="")
+        print("|", end="")
+        for i in range(0, 14):  # Điểm môn học 1
             print("-", end="")
         print("|")
 
     elif flag_diem_so is True:
-        for i in range(0, 13):
+        for i in range(0, 13):  # Điểm môn 1
             print("-", end="")
         print("|", end="")
-        for i in range(0, 14):
+        for i in range(0, 14):  # Điểm môn 2
             print("-", end="")
         print("|")
 
     elif flag_danh_sach is True:
-        for i in range(0, 16):
+        for i in range(0, 16):  # Mã môn 1
             print("-", end="")
         print("|", end="")
-        for i in range(0, 14):
+        for i in range(0, 14):  # Điểm môn 1
             print("-", end="")
         print("|", end="")
-        for i in range(0, 16):
+        for i in range(0, 16):  # Mã môn 2
             print("-", end="")
         print("|", end="")
-        for i in range(0, 14):
+        for i in range(0, 14):  # Điểm môn 2
             print("-", end="")
         print("|")
 
@@ -72,25 +82,34 @@ def table_draw():
                   '{:^12}'.format(list_dm1[entries]), "|",
                   '{:^14}'.format(list_mmh2[entries]), "|",
                   '{:^12}'.format(list_dm2[entries]), "|")
-        xem_danh_sach.initialize()
 
     else:
-        from diem_so import np_mshv, np_mshv_sort, np_hthv_sort
-        for num_entries in range(0, len(np_mshv)):
-            print("|", '{:^14}'.format(np_mshv_sort[num_entries]), "|",
-                  '{:<30}'.format(np_hthv_sort[num_entries]), "|", end="")
+        if flag_mmh1 is True or flag_mmh2 is True:
+            from diem_so import list_mshv, list_hthv
+            for entries in range(0, len(list_mshv)):
+                print("|", '{:^14}'.format(list_mshv[entries]), "|",
+                      '{:<30}'.format(list_hthv[entries]), "|", end="")
+                if flag_mmh1 is True:
+                    from diem_so import list_mmh1, list_dm1
+                    print('{:^15}'.format(list_mmh1[entries]), "|",
+                          '{:^12}'.format(list_dm1[entries]), "|")
+                elif flag_mmh2 is True:
+                    from diem_so import list_mmh2, list_dm2
+                    print('{:^15}'.format(list_mmh2[entries]), "|",
+                          '{:^12}'.format(list_dm2[entries]), "|")
 
-            if flag_dm1 is True:
-                from diem_so import np_dm1
-                print('{:^12}'.format(np_dm1[num_entries]), "|")
-
-            elif flag_dm2 is True:
-                from diem_so import np_dm2
-                print('{:^12}'.format(np_dm2[num_entries]), "|")
-
-            elif flag_diem_so is True:
-                from diem_so import np_dm1_sort, np_dm2_after_dm1_sort
-                print('{:^12}'.format(np_dm1_sort[num_entries]), "|",
-                      '{:^12}'.format(np_dm2_after_dm1_sort[num_entries]), "|")
-
-        diem_so.initialization()
+        else:
+            from diem_so import np_mshv_sort, np_hthv_sort
+            for num_entries in range(0, len(np_mshv_sort)):
+                print("|", '{:^14}'.format(np_mshv_sort[num_entries]), "|",
+                      '{:<30}'.format(np_hthv_sort[num_entries]), "|", end="")
+                if flag_dm1 is True:
+                    from diem_so import np_dm1
+                    print('{:^12}'.format(np_dm1[num_entries]), "|")
+                elif flag_dm2 is True:
+                    from diem_so import np_dm2
+                    print('{:^12}'.format(np_dm2[num_entries]), "|")
+                elif flag_diem_so is True:
+                    from diem_so import np_dm1_sort, np_dm2_after_dm1_sort
+                    print('{:^12}'.format(np_dm1_sort[num_entries]), "|",
+                          '{:^12}'.format(np_dm2_after_dm1_sort[num_entries]), "|")
