2762ad8338de33241c735fa2d8e8cc57f775564a
al3xzaytev <ieatsyrniks@gmail.com>
2024-04-29 00:17:07 +0700

    Chỉnh sửa một số tác vụ nhỏ (không vào vòng lặp khi xoá học viên nữa)

--- xoa_hoc_vien.py
+++ xoa_hoc_vien.py
@@ -9,29 +9,28 @@ def xoa_hoc_vien():
     danh_sach = file_hoc_vien.readlines()
     file_path = file_hoc_vien.name
 
-    while True:
-        found = False
-        mshv_input = input("Nhập mã số học viên để xoá: ")
+    found = False
+    mshv_input = input("Nhập mã số học viên để xoá: ")
 
-        if mshv_input == "quit":
-            break
-        else:
-            for entry in danh_sach:
-                mshv_entry = entry.split(sep="|")
+    if mshv_input == "quit":
+        return None
+    else:
+        for entry in danh_sach:
+            mshv_entry = entry.split(sep="|")
 
-                if mshv_entry[0] == mshv_input:
-                    print(f"Đã tìm thấy {mshv_input}.")
-                    danh_sach.remove(entry)
-                    found = True
-                    file_hoc_vien.close()
-                else:
-                    continue
+            if mshv_entry[0] == mshv_input:
+                print(f"Đã tìm thấy {mshv_input}.")
+                danh_sach.remove(entry)
+                found = True
+                file_hoc_vien.close()
+            else:
+                continue
 
-            if found:
-                file_modded = open(file_path, "w")
-                for entries in danh_sach:
-                    file_modded.write(entries)
-                file_modded.close()
-                print("Đã xoá", mshv_input, "khỏi danh sách.")
-            elif not found:
-                print("Không thể tìm thấy mã số học viên", mshv_input, "trong danh sách.")
+        if found:
+            file_modded = open(file_path, "w")
+            for entries in danh_sach:
+                file_modded.write(entries)
+            file_modded.close()
+            print("Đã xoá", mshv_input, "khỏi danh sách.")
+        elif not found:
+            print("Không thể tìm thấy mã số học viên", mshv_input, "trong danh sách.")
