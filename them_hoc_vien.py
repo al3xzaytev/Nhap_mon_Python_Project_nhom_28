bc0b6cd45230b1266edf8cfbb07db4f7721f7e98
al3xzaytev <ieatsyrniks@gmail.com>
2024-04-30 00:46:19 +0700

    Thêm lệnh quit khi đang nhập; Thêm các điều kiện nhập thông tin học viên hợp lệ

--- them_hoc_vien.py
+++ them_hoc_vien.py
@@ -9,23 +9,61 @@ def them_hoc_vien():
 
     print("\nNhập thông tin học viên.")
 
-    mshv = int(input("Nhập mã số học viên: "))
-    file.write(f"{mshv}|")
+    while True:
+        mshv = input("Nhập mã số học viên: ")
+        if mshv == "quit":
+            return None
+        elif len(mshv) == 8:
+            break
+        else:
+            print("Lỗi: Mã số học viên không hợp lệ!\n")
 
     hthv = str(input("Nhập họ tên học viên (không dấu): "))
+    if hthv == "quit":
+        return None
+
+    while True:
+        mmh1 = str(input("Nhập mã môn học 1 (4 chữ cái): "))
+        if mmh1 == "quit":
+            return None
+        elif len(mmh1) == 4:
+            break
+        else:
+            print("Lỗi: Mã môn học không hợp lệ!\n")
+
+    while True:
+        dm1 = input("Nhập điểm môn học 1 (số nguyên từ 0-10): ")
+        if dm1 == "quit":
+            return None
+        elif dm1.isdigit() and 0 <= int(dm1) <= 10:
+            break
+        else:
+            print("Lỗi: Điểm không hợp lệ!\n")
+
+    while True:
+        mmh2 = str(input("Nhập mã môn học 2 (4 chữ cái): "))
+        if mmh2 == "quit":
+            return None
+        elif len(mmh2) == 4:
+            break
+        else:
+            print("Lỗi: Mã môn học không hợp lệ!\n")
+
+    while True:
+        dm2 = input("Nhập điểm môn học 2 (số nguyên từ 0-10): ")
+        if dm2 == "quit":
+            return None
+        elif dm2.isdigit() and 0 <= int(dm2) <= 10:
+            file.close()
+            break
+        else:
+            print("Lỗi: Điểm không hợp lệ!\n")
+
+    file.write(f"{mshv}|")
     file.write(f"{hthv}|")
-
-    mmh1 = str(input("Nhập mã môn học 1 (4 chữ cái): "))
     file.write(f"{mmh1}|")
-
-    dm1 = int(input("Nhập điểm môn học 1 (số nguyên từ 0-10): "))
     file.write(f"{dm1}|")
-
-    mmh2 = str(input("Nhập mã môn học 2 (4 chữ cái): "))
     file.write(f"{mmh2}|")
-
-    dm2 = int(input("Nhập điểm môn học 2 (số nguyên từ 0-10): "))
     file.write(f"{dm2}\n")
-    file.close()
 
     print("Thêm học viên thành công.")
